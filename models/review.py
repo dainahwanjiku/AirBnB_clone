b3adb5403052aabd94d195b8cf579194761be31d
dainahwanjiku <deeshish2017@gmail.com>
2022-08-07 01:54:23 +0300

    review

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module review"""
+
+from modules.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """class review"""
+    place_id = ""
+    user_id = "" 
+    text = ""
