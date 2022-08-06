6a90e1a53ab984cb55c30755964d8a8f2b10d5be
dainahwanjiku <deeshish2017@gmail.com>
2022-08-06 04:10:37 +0300

    user file

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class user
+"""
+
+from models.base_model import BaseModel
+ 
+
+class User(BaseModel):
+    """ user class"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ init user"""
+        super().__init__(*args, **kwargs)
