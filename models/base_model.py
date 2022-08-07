02a0da1f1c636f2142b1711eb941e1a2c20ef436
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-08-08 00:15:02 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,50 @@
 #!/usr/bin/python3
-"""
-BaseModel that defines all common attributes/methods for other classes.
-"""
-
-import uuid
-from json import JSONEncoder
+"""Defines the BaseModel class."""
 import models
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """
-    base model
-    """
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """initialization of the base model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    if key != "__class__":
-                        setattr(self, key, value)
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time_format)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """return dictionary representaton of the instance"""
-        dict_repr = {}
-        for key, value in self.__dict__.items():
-            dict_repr[key] = value
-            if isinstance(value, datetime):
-                dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        dict_repr["__class__"] = type(self).__name__
-        return dict_repr
-
-    def delete(self):
-        """Delete the current instance from storage."""
-        models.storage.delete(self)
+        """Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
 
     def __str__(self):
-        """return the string formated message when instance is called"""
-        clName = self.__class__.__name__
-        return "[{}] ({}) {}".format(clName, self.id, self.__dict__)
-
-
-class BaseModelEncoder(JSONEncoder):
-    """JSON Encoder for BaseModel
-    """
-
-    def default(self, o):
-        """ default"""
-        if isinstance(o, BaseModel):
-            return o.to_dict()
-        return super().default(o)
+        """Return the print/str representation of the BaseModel instance."""
+        cls_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
+        
